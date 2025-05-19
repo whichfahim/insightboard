4d2db7367531086b3f370214683d3067eb94d8ba
Fahim Imtiaz <fahim.imtiaz07@gmail.com>
2025-05-20 00:58:26 +0600

    charts | added traffic sources
    hardcoded data

--- src/app/pages/dashboard/widgets/traffic-sources.component.ts
+++ src/app/pages/dashboard/widgets/traffic-sources.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-traffic-sources',
   imports: [],
-  template: '<p>Traffic sources content</p>',
+  template: `
+    <div class="chart-container">
+      <canvas #chart></canvas>
+    </div>
+  `,
   styles: '',
 })
-export class TrafficSourcesComponent {}
+export class TrafficSourcesComponent {
+  chart = viewChild.required<ElementRef>('chart');
+
+  ngOnInit() {
+    new Chart(this.chart().nativeElement, {
+      type: 'pie',
+      data: {
+        labels: ['Direct', 'Referral', 'Others'],
+        datasets: [
+          {
+            label: 'Traffic Sources',
+            data: [300, 50, 100],
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)',
+            ],
+            hoverOffset: 4,
+          },
+        ],
+      },
+      options: {
+        maintainAspectRatio: false,
+      },
+    });
+  }
+}
